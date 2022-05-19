c115275088d1a18b5390db5ed0c40f3a272bcbfb
tinachats <marshallchaterera@gmail.com>
2022-05-19 14:25:55 +0200

    Updates to the code base

--- main.py
+++ main.py
@@ -7,25 +7,26 @@ def menu():
     option = int(input('Enter your option: '))
     
     if option == 1:
-        settings()
+        temp_settings()
     elif option == 2:
-        settings()
+        moisture_settings()
     elif option == 3:
-        settings()
+        light_settings()
     else:
         quit()
         
-def settings():
+def temp_settings():
     temp = float(input('Enter your green house temperature value: '))
-    moisture = float(input('Enter your green house moisture value: '))
-    light = float(input('Enter your green house light intensity value: '))
-    set_values = {
-        'temp': temp,
-        'moisture': moisture,
-        'light': light
-    }
-    return set_values
+    print(f'Temp: {temp}° successfully set.')
 
+def moisture_settings():
+    moisture = float(input('Enter your green house moisture value: '))
+    print(f'Moisture: {moisture}% successfully set.')
+    
+def light_settings():
+    light = float(input('Enter your green house light intensity value: '))
+    print(f'Light Intensity: {light}% successfully set.')
+    
 def display():
     welcome()
     menu()
